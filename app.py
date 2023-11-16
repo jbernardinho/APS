4fd87d053938033421e80961397ad2b071bdc790
Jorge Bernardo <jbernardinho@hotmail.com.br>
2023-11-16 13:09:33 -0300

    APS ID

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ CORS(app)
 
 Artigos = [
     {
-            'id': 1,
+            'id': '1',
             'título': 'A Seca na Amazônia: Uma Ameaça para Todo o Brasil?',
             'autor': 'Bruna Soldera'
     },
     {
-            'id': 2,
+            'id': '2',
             'título': 'Dessalinização da água do mar: afinal, esse projeto é viável?',
             'autor': 'Bruna Soldera'
     },
     {
-            'id': 3,
+            'id': '3',
             'título': 'GASES DE EFEITO ESTUFA E POLUIÇÃO DO AR: SEMELHANÇAS E DIFERENCIAIS', 
             'autor': 'Leandro Jose Barbosa Lima'
     },
