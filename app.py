a4aa37bdc663627d429a365f9e39064df6ac07e3
Jorge Bernardo <jbernardinho@hotmail.com.br>
2023-11-16 14:56:02 -0300

    CORRECAO TITULO

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ CORS(app)
 Artigos = [
     {
             'id': '1',
-            'título': 'A Seca na Amazônia: Uma Ameaça para Todo o Brasil?',
+            'titulo': 'A Seca na Amazônia: Uma Ameaça para Todo o Brasil?',
             'autor': 'Bruna Soldera'
     },
     {
             'id': '2',
-            'título': 'Dessalinização da água do mar: afinal, esse projeto é viável?',
+            'titulo': 'Dessalinização da água do mar: afinal, esse projeto é viável?',
             'autor': 'Bruna Soldera'
     },
     {
             'id': '3',
-            'título': 'GASES DE EFEITO ESTUFA E POLUIÇÃO DO AR: SEMELHANÇAS E DIFERENCIAIS', 
+            'titulo': 'GASES DE EFEITO ESTUFA E POLUIÇÃO DO AR: SEMELHANÇAS E DIFERENCIAIS', 
             'autor': 'Leandro Jose Barbosa Lima'
     },
 ]
@@ -57,3 +57,5 @@ def excluir_artigos(id):
 
 
 app.run(port=5000,host='0.0.0.0',debug=True)
+
+
